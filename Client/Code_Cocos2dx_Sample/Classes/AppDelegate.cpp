f1290e8221b0a3611618cf6310472f6526707765
lkstudio.dante <lkstudio.dante@gmail.com>
2024-07-22 03:07:13 +0900

    no message

--- Client/Code_Cocos2dx_Sample/Classes/AppDelegate.cpp
+++ Client/Code_Cocos2dx_Sample/Classes/AppDelegate.cpp
@@ -48,8 +48,7 @@ static cocos2d::Size mediumResolutionSize = cocos2d::Size(1024, 768);
 static cocos2d::Size largeResolutionSize = cocos2d::Size(2048, 1536);
 
 AppDelegate::AppDelegate()
-{
-}
+{ }
 
 AppDelegate::~AppDelegate()
 {
